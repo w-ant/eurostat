0adf17b5584648176801a23481a960d8cf7e5e84
w-ant <wozniak.antoni@gmail.com>
2019-04-02 11:08:18 +0100

    Dockerize app

--- api/app.py
+++ api/app.py
@@ -5,9 +5,12 @@ import pandas as pd
 app = Flask(__name__)
 
 
+@app.route('/')
+def home():
+    return '<h1>Eurostat</h1>'
 @app.route('/countries')
 def list_of_countries():
-    db = sqlite3.connect('./data/data.db')
+    db = sqlite3.connect('data.db')
     cursor = db.cursor()
     query = 'SELECT DISTINCT DECLARANT_ISO FROM data'
     result = [item[0] for item in cursor.execute(query).fetchall()]
@@ -18,7 +21,7 @@ def list_of_countries():
 @app.route('/data/<country_code>/<trade_type>')
 def country(country_code, trade_type):
 
-    db = sqlite3.connect('./data/data.db')
+    db = sqlite3.connect('data.db')
 
     sql_query = 'SELECT PERIOD, SUM(VALUE_EUR) total FROM data  WHERE DECLARANT_ISO=? and TRADE_TYPE=? and PERIOD NOT LIKE "%52" GROUP BY PERIOD ORDER BY PERIOD'
     cc = country_code.upper()
@@ -36,4 +39,4 @@ def country(country_code, trade_type):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
